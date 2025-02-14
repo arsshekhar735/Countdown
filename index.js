7e5c49d9e30e2abfa4b2a98aed2e0f5aafee1249
Suman Shekhar <suman.nwd03@gmail.com>
2025-02-14 16:42:06 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const endDate = "09 March 2025 01:00PM"
+const endDate = "09 March 2025 01:09PM"
+const msg = document.getElementById('final1')
 
 let date = document.getElementById('end-date')
+const inputs = document.querySelectorAll('input')
 date.innerHTML= endDate
 
 
@@ -9,10 +11,26 @@ const clock = () =>{
     const now = new Date()
     // console.log(end,now)
     const diff = (end - now)/1000;
-    console.log(diff)
+    // console.log(diff)
     //We will calculate Days
+   if(diff>0)
+   {
+    inputs[0].value=Math.floor(diff/ 3600 /24);
+    inputs[1].value = Math.floor(diff/3600)%24;
+    inputs[2].value = Math.floor(diff/60)%60;
+    inputs[3].value = Math.floor(diff%60)
+   }
+   else{
+    // console.log("Yoh have reached Home.")
+    msg.innerHTML = "You have reached Home.";
     
-
+   }
+  
 }
 
-clock()
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
