94f4e58d4f68c4110065af08e8b587ae9cf28e32
Suman Shekhar <suman.nwd03@gmail.com>
2025-02-14 15:08:46 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const endDate = 
+const endDate = "09 March 2025 01:00PM"
+
+let date = document.getElementById('end-date')
+date.innerHTML= endDate
+
+
+const clock = () =>{
+    const end = new Date(endDate)
+    const now = new Date()
+    // console.log(end,now)
+    const diff = (end - now)/1000;
+    console.log(diff)
+    //We will calculate Days
+    
+
+}
+
+clock()
